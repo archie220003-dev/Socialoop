fdf40ce9ca304eb84d93696728e2da490711bad8
archie220003-del <archie220003@gmail.com>
2026-04-05 22:09:18 +0530

    fix

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,15 +1,9 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true
 });
 
 export default cloudinary;
-
-
-console.log("CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API_KEY:", process.env.CLOUDINARY_API_KEY);
-console.log("API_SECRET:", process.env.CLOUDINARY_API_SECRET);
